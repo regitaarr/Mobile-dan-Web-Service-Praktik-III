07bac04221b7a769bc4ed4a33912469e3cb673ab
Regita Cahya Arrahma <122368250+rgtamaaa@users.noreply.github.com>
2024-09-29 08:23:02 +0700

    pertemuan 2

---
+++ project/lib/column.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quick Note',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const ColumnPage(),
+    );
+  }
+}
+
+class ColumnPage extends StatefulWidget {
+  const ColumnPage({super.key});
+
+  @override
+  State<ColumnPage> createState() => _ColumnPageState();
+}
+
+class _ColumnPageState extends State<ColumnPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Column',
+          style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600),
+        ),
+      ),
+      body: SafeArea(
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 100.0,
+              height: 100.0,
+              color: Colors.black,
+            ),
+            const SizedBox(height: 16.0),
+            Container(
+              width: 100.0,
+              height: 100.0,
+              color: Colors.blue,
+            ),
+            const SizedBox(
+              height: 16.0,
+            ),
+            Container(
+              width: 100.0,
+              height: 100.0,
+              color: Colors.pink,
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+}
